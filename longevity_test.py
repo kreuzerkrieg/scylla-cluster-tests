bc2329fb655f78247a0acfd48e159b928628a836
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2026-05-05 14:14:44 +0300

    wip

--- longevity_test.py
+++ longevity_test.py
@@ -162,15 +162,15 @@ class LongevityTest(ClusterTester, loader_utils.LoaderUtilsMixin):
         if tombstone_gc_verification_params := self._get_tombstone_gc_verification_params():
             self.run_tombstone_gc_verification_thread(**tombstone_gc_verification_params)
 
-        for node in self.db_cluster.nodes:
-            balancing_cmd = build_node_api_command(
-                '/storage_service/tablets/balancing?enabled=false',
-                RequestMethods.POST)
-            node.remoter.run(balancing_cmd, ignore_status=True, verbose=True)
-
-        compaction_ops = CompactionOps(cluster=self.db_cluster)
-        for node in self.db_cluster.nodes:
-            compaction_ops.disable_autocompaction_on_ks_cf(node=node)
+        # for node in self.db_cluster.nodes:
+        #     balancing_cmd = build_node_api_command(
+        #         '/storage_service/tablets/balancing?enabled=false',
+        #         RequestMethods.POST)
+        #     node.remoter.run(balancing_cmd, ignore_status=True, verbose=True)
+        #
+        # compaction_ops = CompactionOps(cluster=self.db_cluster)
+        # for node in self.db_cluster.nodes:
+        #     compaction_ops.disable_autocompaction_on_ks_cf(node=node)
 
         self.run_prepare_write_cmd()
 
